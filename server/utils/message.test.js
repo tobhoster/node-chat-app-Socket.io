33b9fd5461031f6fc1bd4306e1d43ac0e357494b
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2017-08-29 00:20:16 -0400

    Using geolocation - Web APIs | MDN; Ran test and passed

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -9,4 +9,14 @@ describe('generateMessage', () => {
         expect(res.text).toEqual('How are you?');
         expect(res.createdAt).toBeA("number");
     });
-})
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var res = generateLocationMessage('Admin', '40.7953745', '-73.5022512');
+
+        expect(res.from).toEqual('Admin');
+        expect(res.url).toEqual(`https://www.google.com/maps?q=40.7953745,-73.5022512`);
+        expect(res.createdAt).toBeA("number");
+    });
+});
