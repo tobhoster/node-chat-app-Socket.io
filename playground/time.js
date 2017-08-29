292c56793497d7b6b546c614d36a9bd14a7810a7
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2017-08-29 02:49:02 -0400

    Printing Timestamps

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+
+// var date = new Date();
+// var months = ['Jan', 'Feb'];
+// console.log(date.getMonth());
+
+// var date = moment();
+// console.log(date.format('MMMM Do YYYY, h:mm:ss a'));
+
+// console.log(date.format('h:mm a'));
