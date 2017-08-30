0f2b5de45cc1cf951bfe7a4cdb26bf95d28b190f
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2017-08-30 00:18:39 -0400

    Sending Messages to Room Only
    Also setup Notification

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,6 +16,32 @@ function scrollToBottom() {
     }
 }
 
+function notifyMe(messages) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(messages);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(messages);
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you 
+  // want to be respectful there is no need to bother them any more.
+}
+
 socket.on('connect', () => {
     console.log('Connected to Server');
     var params = jQuery.deparam(window.location.search);
@@ -56,6 +82,7 @@ socket.on('newMessage', (message) => {
 
     jQuery('#messages').append(html);
     scrollToBottom();
+    notifyMe(`${message.from} | ${formattedTime} : ${message.text}`);
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} | ${formattedTime}: ${message.text}`);
 
@@ -80,6 +107,7 @@ socket.on('newLocationMessage', (message) => {
      
     jQuery('#messages').append(html);
     scrollToBottom();
+    notifyMe(`${message.from} | ${formattedTime} : Current Location`);
 })
 
 jQuery('#message-form').on('submit', (e) => {
@@ -88,7 +116,6 @@ jQuery('#message-form').on('submit', (e) => {
     var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, () => {
         messageTextbox.val('');
