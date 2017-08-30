7e770b3160ebfc9c618e255bf5d02bc2537ae998
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2017-08-30 01:16:05 -0400

    async-promises.js

---
+++ playground/async-await/async-promises.js
@@ -0,0 +1,69 @@
+const users = [{
+    id: 1,
+    name: 'Andrew',
+    schoolId: 101
+}, {
+    id: 2,
+    name: 'Jessica',
+    schoolId: 999
+}];
+
+const grades = [{
+    id: 1,
+    schoolId: 101,
+    grade: 86
+}, {
+    id: 2,
+    schoolId: 999,
+    grade: 100
+}, {
+    id: 3,
+    schoolId: 101,
+    grade: 80
+}];
+
+const getUsers = (id) => {
+    return new Promise((resolve, reject) => {
+        const user = users.find((user) => {
+            return user.id === id;
+        });
+
+        if(user) {
+            resolve(user);
+        } else {
+            reject(`Unable to find user with id of ${id}.`);
+        }
+    });
+};
+
+const getGrades = (schoolId) => {
+    return new Promise((resolve, reject) => {
+        resolve(grades.filter((grade) => grade.schoolId === schoolId));
+    })
+};
+
+// Andrew has a 83% in the class
+const getStatus = (userId) => {
+    return getUsers(userId).then((tempUser) => {
+        user = tempUser;
+        return getGrades(user.schoolId);
+    }).then((grades) => {
+        let average = 0;
+
+        if(grades.length > 0) {
+            average = grades.map((grade) => grade.grade).reduce((a, b) => {
+
+                return a + b;
+            });
+            average /= grades.length;
+
+            return `${user.name} has a ${average}% in the class.`
+        }
+    })
+}
+
+getStatus(1).then((status) => {
+    console.log(status);
+}).catch((err) => {
+    console.log(err);
+})
